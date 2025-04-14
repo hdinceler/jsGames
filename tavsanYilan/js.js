14b4214839940acac65e146d654c0c78ea93d96a
hdinceler <55253026+hdinceler@users.noreply.github.com>
2025-04-14 20:09:43 +0300

    Add files via upload

---
+++ tavsanYilan/js.js
@@ -0,0 +1,57 @@
+window.onload=()=>{
+    let puan=0;
+    let can=3;
+    const skorTablosu=document.getElementById('skorTablosu');
+    const puanlar=document.getElementById('puanlar');
+    const canlar=document.getElementById('canlar');
+
+     const yilan=document.getElementById("yilan");
+     const havuc1=document.getElementById("havuc1");
+     const havuc2=document.getElementById("havuc2");
+     const havuc3=document.getElementById("havuc3");
+
+     canYaz=(adet)=>{
+        console.log(can);
+        canlar.innerHTML=""
+        for(let i=1; i<=adet;i++){
+            const tavsan=document.createElement('img');
+            tavsan.src='./rabbit.png';
+            tavsan.height=54;
+            canlar.appendChild(tavsan);
+        }
+     }
+
+
+     havuc1.addEventListener(
+        'click' , ()=>{ 
+            puan+=10;
+            skorTablosu.innerText=puan + " PUAN"
+            canYaz(can)
+         }
+     );
+     havuc2.addEventListener(
+        'click' , ()=>{ 
+            puan+=20;
+            skorTablosu.innerText=puan + " PUAN"
+            canYaz(can)
+         }
+     );
+
+     havuc3.addEventListener(
+        'click' , ()=>{ 
+            puan+=30;
+            skorTablosu.innerText=puan + " PUAN"
+            canYaz(can)
+         }
+     );
+     yilan.addEventListener(
+        'click' , ()=>{ 
+            puan=0;
+            skorTablosu.innerText=puan + " PUAN";
+            can--
+            if(can <=0)
+            canYaz(can)
+         }
+     );
+
+}
