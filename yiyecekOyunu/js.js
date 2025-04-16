b6e5b8f69177e6a08adac8a9cb69e98b27e5b0e5
hdinceler <55253026+hdinceler@users.noreply.github.com>
2025-04-16 17:52:38 +0300

    Delete yiyecekOyunu directory

--- yiyecekOyunu/js.js
+++
@@ -1,8 +0,0 @@
-const buton1=document.getElementById("buton1")
-buton1.addEventListener('click', ()=>{
-    alert("1");
-})
-const buton2=document.getElementById("buton2")
-buton2.addEventListener('click', ()=>{
-    alert("2");
-})
